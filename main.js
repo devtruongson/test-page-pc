b43c85b7b6bb15fe8626a1a0aca2f5f4f7474ec0
devtruongson <truongsonpt.80@gmail.com>
2023-08-12 16:24:12 +0700

    uo

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
 const ToogleMenu = document.querySelector(".toggle-menu");
 const MenuWp = document.querySelector(".meu-mb-wp");
-const Overlay = document.querySelector(".obverlay");
-const BtnClose = document.querySelector(".close-header");
 
 ToogleMenu.addEventListener("click", () => {
-    MenuWp.style.display = "block";
-});
-
-Overlay.addEventListener("click", () => {
-    MenuWp.style.display = "none";
-});
-
-BtnClose.addEventListener("click", () => {
-    MenuWp.style.display = "none";
+    MenuWp.classList.toggle("active-header");
 });
